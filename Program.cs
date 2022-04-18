ce84f69523e06212a5c85727e0ec6a706464bb0c
Areg-ILP <areg-ilp@outlook.com>
2022-04-18 21:10:31 +0400

    Last tcik

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace InSearchOfMiruine
         /// <summary>
         /// Тhis code was hidden from the government. 
         /// but we managed somehow and were able to send it via a secure line.
-        /// Vormzdegh helped us with this).
+        /// Vormzdegh helped us with this.
         /// Thank you for lessons!
         /// </summary>
         static void Main(string[] args)
